c50eb681029c5811f5587f6f5e71af59370bb04b
Astro <98563861+4str0x@users.noreply.github.com>
2022-09-17 00:30:55 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as arq:
     readme = arq.read()
 
 setup(name='squareapi_py',
-    version='0.0.3',
+    version='0.0.4',
     license='MIT License',
     author='Astro',
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='ericksantosyb@gmail.com',
     keywords='square_lib',
-    description=u'Uma biblioteca que consomê a api da square',
-    packages=['squarecloud_py'],
+    description=u'Uma biblioteca que ultilizar a api da squarecloud',
+    packages=['squareapi_py'],
     install_requires=['requests', 'colorama'],)
